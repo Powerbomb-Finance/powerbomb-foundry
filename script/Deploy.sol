8e9ae8b404c5b4f8d7801e2c1db6f302ff53a948
Siew <siewchongche@gmail.com>
2022-08-01 02:14:27 +0800

    chore: framework for interface & script

---
+++ script/Deploy.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "forge-std/Script.sol";
+
+contract Deploy is Script {
+
+    function run() public {
+        vm.startBroadcast();
+
+        // script start here
+    }
+}
