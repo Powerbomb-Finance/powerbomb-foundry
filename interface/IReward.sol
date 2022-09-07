a208fb7c8c1d5152c1c0ef0b10f4d53f0a74ddb2
Siew <siewchongche@gmail.com>
2022-09-08 04:59:56 +0800

    chore: update lib, bump solidity version

--- interface/IReward.sol
+++ interface/IReward.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
+pragma solidity 0.8.16;
 
 interface IReward {
     function recordDeposit(address account, uint amount, address rewardToken) external;
