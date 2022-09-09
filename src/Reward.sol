92e2f93ebc6d015f680fdd2c506a23af3e8ae0e3
Siew <siewchongche@gmail.com>
2022-09-09 22:26:11 +0800

    feat: Reward contract WIP

--- src/Reward.sol
+++ src/Reward.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-contract Reward {
-    //
+import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Reward is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+
+    receive() external payable {}
+    
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
