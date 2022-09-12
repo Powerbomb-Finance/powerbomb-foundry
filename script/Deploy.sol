dd545cdc105c53d3fe385a3928353f234527b1a6
Siew <siewchongche@gmail.com>
2022-09-12 18:51:05 +0800

    chore: deploy to testnet

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.16;
 import "forge-std/Script.sol";
 import "../src/PengTogether.sol";
 import "../src/FarmCurve.sol";
+import "../src/Reward.sol";
 import "../src/PbProxy.sol";
 
 contract Deploy is Script {
@@ -11,25 +12,43 @@ contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
 
-        FarmCurve farm = new FarmCurve();
-        PbProxy farmProxy = new PbProxy(
-            address(farm),
-            abi.encodeWithSelector(bytes4(keccak256("initialize()")))
-        );
-        farm = FarmCurve(payable(address(farmProxy)));
-        console.log("farm:", address(farm));
+        // FarmCurve farm = new FarmCurve();
+        // PbProxy farmProxy = new PbProxy(
+        //     address(farm),
+        //     abi.encodeWithSelector(bytes4(keccak256("initialize()")))
+        // );
+        // farm = FarmCurve(payable(address(farmProxy)));
+        // console.log("farm:", address(farm));
 
-        PengTogether vault = new PengTogether();
-        PbProxy vaultProxy = new PbProxy(
-            address(vault),
+        // PengTogether vault = new PengTogether();
+        // PbProxy vaultProxy = new PbProxy(
+        //     address(vault),
+        //     abi.encodeWithSelector(
+        //         bytes4(keccak256("initialize(address)")),
+        //         address(farm)
+        //     )
+        // );
+        // vault = PengTogether(address(vaultProxy));
+        // console.log("vault:", address(vault));
+
+        // farm.setVault(address(vault));
+
+
+        address pengTogetherVault = 0x1EFB578eCe71D99f5994a79815aA09A8f87F7429;
+        uint64 subscriptionId = 22001;
+        Reward reward = new Reward();
+        PbProxy rewardProxy = new PbProxy(
+            address(reward),
             abi.encodeWithSelector(
-                bytes4(keccak256("initialize(address)")),
-                address(farm)
+                bytes4(keccak256("initialize(address,uint64)")),
+                pengTogetherVault,
+                subscriptionId
             )
         );
-        vault = PengTogether(address(vaultProxy));
-        console.log("vault:", address(vault));
+        reward = Reward(payable(address(rewardProxy)));
+        console.log("reward:", address(reward));
 
-        farm.setVault(address(vault));
+        FarmCurve farm = FarmCurve(payable(0xA6cFCa9EB181728082D35419B58Ba7eE4c9c8d38));
+        farm.setReward(address(reward));
     }
 }
