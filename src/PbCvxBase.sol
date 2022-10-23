9198029ac9ea1bcc5b23abf2981fb3bbe45fd306
Siew <siewchongche@gmail.com>
2022-10-23 17:06:45 +0800

    feat: add usdt dai address

--- src/PbCvxBase.sol
+++ src/PbCvxBase.sol
@@ -21,7 +21,9 @@ abstract contract PbCvxBase is Initializable, UUPSUpgradeable, OwnableUpgradeabl
     IERC20Upgradeable constant cvx = IERC20Upgradeable(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
     IERC20Upgradeable constant wbtc = IERC20Upgradeable(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
     IERC20Upgradeable constant weth = IERC20Upgradeable(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20Upgradeable constant usdt = IERC20Upgradeable(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     IERC20Upgradeable constant usdc = IERC20Upgradeable(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20Upgradeable constant dai = IERC20Upgradeable(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     IBooster constant booster = IBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
     IPool public pool;
     IGauge public gauge;
