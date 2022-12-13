f84a42970a456c884e6dedf19475f0f0aa39b349
Siew <siewchongche@gmail.com>
2022-12-13 15:51:44 +0800

    chore: add comments, remove duplicate functions, initialize wethAmt

--- src/Vault_seth.sol
+++ src/Vault_seth.sol
@@ -8,11 +8,11 @@ contract Vault_seth is PengTogether {
     using SafeERC20Upgradeable for IERC20Upgradeable;
     using SafeERC20Upgradeable for IWETH;
 
-    // sETH/ETH Curve pool
+    // seth/eth Curve pool
     IPool constant pool_seth = IPool(0x7Bc5728BC2b59B45a58d9A576E2Ffc5f0505B35E);
-    // sETH/ETH Curve LP token
+    // seth/eth Curve lp token
     IERC20Upgradeable constant lpToken_seth = IERC20Upgradeable(0x7Bc5728BC2b59B45a58d9A576E2Ffc5f0505B35E);
-    // Curve staking pool for sETH/ETH LP token
+    // Curve staking pool for seth/eth lp token
     IGauge constant gauge_seth = IGauge(0xCB8883D1D8c560003489Df43B30612AAbB8013bb);
     IChainlink constant ethUsdPriceOracle = IChainlink(0x13e3Ee699D1909E989722E753853AE30b17e08c5);
 
@@ -31,6 +31,7 @@ contract Vault_seth is PengTogether {
         op.safeApprove(address(swapRouter), type(uint).max);
     }
 
+    /// @inheritdoc PengTogether
     function deposit(
         IERC20Upgradeable token,
         uint amount,
@@ -39,17 +40,7 @@ contract Vault_seth is PengTogether {
         _deposit(token, amount, amountOutMin, msg.sender);
     }
 
-    function depositByHelper(
-        IERC20Upgradeable token,
-        uint amount,
-        uint amountOutMin,
-        address depositor
-    ) external payable override {
-        require(msg.sender == helper, "helper only");
-
-        _deposit(token, amount, amountOutMin, depositor);
-    }
-
+    /// @inheritdoc PengTogether
     function _deposit(
         IERC20Upgradeable token,
         uint amount,
@@ -60,36 +51,22 @@ contract Vault_seth is PengTogether {
         require(amount >= 0.1 ether, "min 0.1 ether");
         require(amount == msg.value, "amount != msg.value");
 
-        uint[2] memory amounts;
+        uint[2] memory amounts; // [eth, seth]
         amounts[0] = amount;
+        // add liquidity eth into curve pool to get lp token
         uint lpTokenAmt = pool_seth.add_liquidity{value: msg.value}(amounts, amountOutMin);
+        // deposit into curve lp token staking pool for reward
         gauge_seth.deposit(lpTokenAmt);
 
+        // update user info into peng together record contract
         record.updateUser(true, depositor, amount, lpTokenAmt);
+        // save block which call this function to check if withdraw within same block
         depositedBlock[depositor] = block.number;
 
         emit Deposit(depositor, amount, lpTokenAmt);
     }
 
-    function withdraw(
-        IERC20Upgradeable token,
-        uint amount,
-        uint amountOutMin
-    ) external override returns (uint actualAmt) {
-        actualAmt = _withdraw(token, amount, amountOutMin, msg.sender);
-    }
-
-    function withdrawByHelper(
-        IERC20Upgradeable token,
-        uint amount,
-        uint amountOutMin,
-        address account
-    ) external override returns (uint actualAmt) {
-        require(msg.sender == helper, "helper only");
-
-        actualAmt = _withdraw(token, amount, amountOutMin, account);
-    }
-
+    /// @inheritdoc PengTogether
     function _withdraw(
         IERC20Upgradeable token,
         uint amount,
@@ -101,29 +78,36 @@ contract Vault_seth is PengTogether {
         require(depositBal >= amount, "amount > depositBal");
         require(depositedBlock[account] != block.number, "same block deposit withdraw");
 
-        uint withdrawPerc = amount * 1e18 / depositBal;
-        uint lpTokenAmt = lpTokenBal * withdrawPerc / 1e18;
+        // calculate lp token amount to withdraw
+        uint lpTokenAmt = lpTokenBal * amount / depositBal;
+        // withdraw from curve lp token staking pool
         gauge_seth.withdraw(lpTokenAmt);
+        // burn lp token and remove liquidity from curve pool and receive eth
         actualAmt = pool_seth.remove_liquidity_one_coin(lpTokenAmt, 0, amountOutMin);
 
+        // update user info into peng together record contract
         record.updateUser(false, account, amount, lpTokenAmt);
 
         // eth transfer to msg.sender instead of account because
         // for withdraw() eth transfer to caller (depositor)
         // for withdrawByHelper() eth transfer to pengHelperOp
+        // and pengHelperOp help to transfer eth to account
         (bool success,) = msg.sender.call{value: actualAmt}("");
         require(success);
         emit Withdraw(account, amount, lpTokenAmt, actualAmt);
     }
 
+    /// @notice harvest from curve for rewards and sell them for weth
+    /// @inheritdoc PengTogether
     function harvest() external override {
         minter.mint(address(gauge_seth)); // to claim crv
         gauge_seth.claim_rewards(); // to claim op
-        uint wethAmt;
+        uint wethAmt = 0;
 
-        // swap crv to weth
+        // swap crv to weth via uniswap v3
+        // no slippage needed because small amount swap
         uint crvAmt = crv.balanceOf(address(this));
-        if (crvAmt > 1 ether) {
+        if (crvAmt > 1 ether) { // minimum swap 1e18 crv
             wethAmt = swapRouter.exactInputSingle(
                 ISwapRouter.ExactInputSingleParams({
                     tokenIn: address(crv),
@@ -138,14 +122,15 @@ contract Vault_seth is PengTogether {
             );
         }
 
-        // swap op to weth
+        // swap op to weth via uniswap v3
+        // no slippage needed because small amount swap
         uint opAmt = op.balanceOf(address(this));
-        if (opAmt > 1 ether) {
+        if (opAmt > 1 ether) { // minimum swap 1e18 op
             wethAmt += swapRouter.exactInputSingle(
                 ISwapRouter.ExactInputSingleParams({
                     tokenIn: address(op),
                     tokenOut: address(weth),
-                    fee: 3000,
+                    fee: 3000, // 0.3%
                     recipient: address(this),
                     deadline: block.timestamp,
                     amountIn: opAmt,
@@ -166,31 +151,27 @@ contract Vault_seth is PengTogether {
         emit Harvest(crvAmt, opAmt, wethAmt, fee);
     }
 
+    /// @notice calculate usd value per lp token
+    /// @inheritdoc PengTogether
     function getPricePerFullShareInUSD() public override view returns (uint) {
+        // get latest price from chainlink
         (, int latestPrice,,,) = ethUsdPriceOracle.latestRoundData();
+        // get_virtual_price() return 18 decimals, latestPrice 8 decimals
         return pool_seth.get_virtual_price() * uint(latestPrice) / 1e20; // 6 decimals
     }
 
+    /// @notice get all lp token amount stake in curve farm(gauge)
+    /// @inheritdoc PengTogether
     function getAllPool() public override view returns (uint) {
-        return gauge_seth.balanceOf(address(this)); // lpToken, 18 decimals
+        return gauge_seth.balanceOf(address(this)); // 18 decimals
     }
 
-    function getAllPoolInUSD() external override view returns (uint allPoolInUSD) {
-        uint allPool = getAllPool();
-        if (allPool > 0) {
-            return allPool * getPricePerFullShareInUSD() / 1e18; // 6 decimals
-        }
-    }
-
+    /// @notice get current pending rewards for harvest
     /// @dev Call this function off-chain by using view
+    /// @return crvReward pendig crv reward
+    /// @return opReward pendig op reward
     function getPoolPendingReward() external override returns (uint crvReward, uint opReward) {
         crvReward = gauge_seth.claimable_tokens(address(this));
         opReward = gauge_seth.claimable_reward(address(this), address(op));
-    }
-
-    ///@notice user actual balance in usd after deposit into farm (after slippage), 6 decimals
-    function getUserBalanceInUSD(address account) external override view returns (uint) {
-        (, uint lpTokenBal,,) = record.userInfo(account);
-        return lpTokenBal * getPricePerFullShareInUSD() / 1e18;
     }
 }
