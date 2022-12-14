8281dc5c9ccc23ea028c875c6e05a328c85f8516
Siew <siewchongche@gmail.com>
2022-12-14 15:12:36 +0800

    chore: add more comments, remove unused event

--- src/Reward.sol
+++ src/Reward.sol
@@ -9,35 +9,36 @@ import "openzeppelin-contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "../interface/ILSSVMRouter.sol";
 import "../interface/ISudoPool.sol";
 
-contract Reward is
-    Initializable, 
-    OwnableUpgradeable,
-    UUPSUpgradeable
-{
+/// @title contract for buy nft from sudoswap and transfer to dao contract
+/// @author siew
+contract Reward is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     ILSSVMRouter constant ROUTER = ILSSVMRouter(0x2B2e8cDA09bBA9660dCA5cB6233787738Ad68329); // sudoswap
 
-    mapping(uint16 => bytes) public trustedRemoteLookup; // PengTogether contract on Optimism
+    // to set peng together contract address on optimism, unused now due to eth from stargate
+    // can receive directly in this contract without stargate sgReceive
+    mapping(uint16 => bytes) public trustedRemoteLookup;
     address public admin;
     address public dao;
     uint public nftSwapped;
 
     event BuyNFT(address pool, uint NFTPrice);
-    event SetTrustedRemoteLookup(uint16 chainId, address trustedRemote);
     event SetAdmin(address admin_);
     event SetDao(address dao_);
 
-    function initialize(address dao_, address pengTogetherVault) external initializer {
+    function initialize(address dao_) external initializer {
         require(dao_ != address(0), "address 0");
         __Ownable_init();
-
-        trustedRemoteLookup[111] = abi.encodePacked(pengTogetherVault);
 
         dao = dao_;
         admin = msg.sender;
     }
 
+    /// @notice to receive eth send from stargate router
     receive() external payable {}
 
+    /// @notice buy nft and send to dao contract
+    /// @dev make sure eth in this contract > floor price of nft
+    /// @param pool sudoswap pool to buy nft
     function buyNFT(address pool) external {
         require(msg.sender == admin || msg.sender == owner(), "only authorized");
         nftSwapped += 1;
@@ -59,12 +60,8 @@ contract Reward is
         nftSwapped = nftSwapped_;
     }
 
-    function setTrustedRemoteLookup(uint16 chainId, address trustedRemote) external onlyOwner {
-        trustedRemoteLookup[chainId] = abi.encodePacked(trustedRemote);
-
-        emit SetTrustedRemoteLookup(chainId, trustedRemote);
-    }
-
+    /// @notice set new admin
+    /// @param admin_ new admin address
     function setAdmin(address admin_) external onlyOwner {
         require(admin_ != address(0), "address 0");
         admin = admin_;
@@ -72,6 +69,8 @@ contract Reward is
         emit SetAdmin(admin_);
     }
 
+    /// @notice set new dao contract
+    /// @param dao_ new dao contract address
     function setDao(address dao_) external onlyOwner {
         require(dao_ != address(0), "address 0");
         dao = dao_;
@@ -79,6 +78,11 @@ contract Reward is
         emit SetDao(dao_);
     }
 
+    /// @notice get pool with floor price from input pools
+    /// @param pools pool list to query
+    /// @param nft nft to query
+    /// @return floorPrice floor price of the selected pool
+    /// @return poolWithFloorPrice pool selected with floor price
     function getPoolWithFloorPrice(
         address[] calldata pools,
         address nft
@@ -86,12 +90,16 @@ contract Reward is
         for (uint i = 0; i < pools.length; i++) {
             ISudoPool pool = ISudoPool(pools[i]);
             if (IERC721Upgradeable(nft).balanceOf(address(pool)) > 0) {
+                // fetch price of the pool
                 (,,, uint inputAmount,) = pool.getBuyNFTQuote(1);
-                if (floorPrice == 0) {
+                if (i == 0) {
+                    // first pool
                     floorPrice = inputAmount;
                     poolWithFloorPrice = address(pool);
                 } else {
                     if (inputAmount < floorPrice) {
+                        // current pool floor price is lower than previous floor price
+                        // update current pool floor price and pool address
                         floorPrice = inputAmount;
                         poolWithFloorPrice = address(pool);
                     }
@@ -100,5 +108,6 @@ contract Reward is
         }
     }
 
+    /// @dev for uups upgradeable
     function _authorizeUpgrade(address) internal override onlyOwner {}
 }
