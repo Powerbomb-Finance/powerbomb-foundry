d8add3f1b95eb9e738d26a32ea68e59ea3de2236
Siew <siewchongche@gmail.com>
2022-12-16 15:51:54 +0800

    chore: add more comments, formatting, rename _lastSeat to totalSeat

--- src/Record_eth.sol
+++ src/Record_eth.sol
@@ -5,24 +5,26 @@ import "./Record.sol";
 
 contract Record_eth is Record {
 
-    function _updateTicketAmount(address _user) internal override {
-        User storage user = userInfo[_user];
+    /// @inheritdoc Record
+    function _updateTicketAmount(address user_) internal override {
+        User storage user = userInfo[user_];
         uint depositHour = (block.timestamp - user.lastUpdateTimestamp) / 3600;
         uint depositInZeroPointOneEther = user.depositBal / 0.1 ether;
         if (depositHour > 0 && depositInZeroPointOneEther > 0) {
             user.ticketBal += depositHour * depositInZeroPointOneEther; // 1 deposit hour * 0.1 ether = 1 ticket
-            emit UpdateTicketAmount(_user, depositHour, depositInZeroPointOneEther);
+            emit UpdateTicketAmount(user_, depositHour, depositInZeroPointOneEther);
         }
         user.lastUpdateTimestamp = block.timestamp;
     }
 
-    function _placeSeat(address user, uint _lastSeat) internal override returns (uint) {
+    /// @inheritdoc Record
+    function _placeSeat(address user, uint totalSeats) internal override returns (uint) {
         _updateTicketAmount(user);
 
         if (userInfo[user].depositBal > 0.1 ether) {
             uint ticket = userInfo[user].ticketBal;
             if (ticket > 0) {
-                uint from = _lastSeat;
+                uint from = totalSeats;
                 uint to = from + ticket - 1;
 
                 seats.push(Seat({
@@ -32,7 +34,7 @@ contract Record_eth is Record {
                 }));
 
                 userInfo[user].ticketBal = 0;
-                _lastSeat += ticket;
+                totalSeats += ticket;
 
                 emit PlaceSeat(user, from, to, seats.length - 1);
             }
@@ -41,19 +43,19 @@ contract Record_eth is Record {
             userInfo[user].ticketBal = 0;
         }
 
-        return _lastSeat;
+        return totalSeats;
     }
 
-    function getUserAvailableTickets(address _user) public override view returns (uint ticket) {
-        // get user pending ticket (not yet place seat)
-        User memory user = userInfo[_user];
-        uint pendingTicket = user.ticketBal;
+    /// @inheritdoc Record
+    function getUserAvailableTickets(address user_) public override view returns (uint ticket) {
+        User memory user = userInfo[user_];
 
-        // get user available ticket to claim
         uint depositHour = (block.timestamp - user.lastUpdateTimestamp) / 3600;
         uint depositInZeroPointOneEther = user.depositBal / 0.1 ether;
         uint availableTicket = depositHour * depositInZeroPointOneEther;
 
-        return pendingTicket + availableTicket;
+        uint pendingTicket = user.ticketBal;
+
+        return availableTicket + pendingTicket;
     }
 }
