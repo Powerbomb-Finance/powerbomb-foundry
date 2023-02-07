fad0ca4f57688b2f81748d47e5b468997d88e8a7
Siew <siewchongche@gmail.com>
2023-02-07 22:54:39 +0800

    chore: prepare for upgrade final contract

--- script/Upgrade.sol
+++ script/Upgrade.sol
@@ -3,16 +3,17 @@ pragma solidity 0.8.16;
 
 import "forge-std/Script.sol";
 // import "../src/PengTogether.sol";
+import "../src/PengTogether_final.sol";
 // import "../src/Vault_seth.sol";
 // import "../src/Record.sol";
 // import "../src/Record_eth.sol";
 // import "../src/Reward.sol";
 // import "../src/Dao.sol";
 // import "../src/PengHelperEth.sol";
-import "../src/PengHelperOp.sol";
+// import "../src/PengHelperOp.sol";
 
 contract Upgrade is Script {
-    // PengTogether vault = PengTogether(payable(0x68ca3a3BBD306293e693871E45Fe908C04387614));
+    PengTogether_final vault = PengTogether_final(payable(0x68ca3a3BBD306293e693871E45Fe908C04387614));
     // Vault_seth vault_seth = Vault_seth(payable(0x98f82ADA10C55BC7D67b92d51b4e1dae69eD0250));
     // Record record = Record(0x176B6aD5063bFFBca9867DE6B3a1Eb27A306e40d);
     // Record_eth record_eth = Record_eth(0xC530677144A7EA5BaE6Fbab0770358522b4e7071);
@@ -20,19 +21,13 @@ contract Upgrade is Script {
     // Reward reward = Reward(payable(0xc052Ac7d4c68fA03b2cAf2A12B745fB6B8eC08Dd));
     // Dao dao = Dao(0x0C9133Fa96d72C2030D63B6B35c3738D6329A313);
     // PengHelperEth pengHelperEth = PengHelperEth(payable(0x8799c7fEfB44B8c885b489eB38Fb067c75EbA2ab));
-    PengHelperOp pengHelperOp = PengHelperOp(payable(0xCf91CDBB4691a4b912928A00f809f356c0ef30D6));
+    // PengHelperOp pengHelperOp = PengHelperOp(payable(0xCf91CDBB4691a4b912928A00f809f356c0ef30D6));
 
     function run() public {
         vm.startBroadcast();
 
-        // PengTogether vaultImpl = new PengTogether();
-        // vault.upgradeToAndCall(
-        //     address(vaultImpl),
-        //     abi.encodeWithSelector(
-        //         bytes4(keccak256("setHelper(address)")),
-        //         0xCf91CDBB4691a4b912928A00f809f356c0ef30D6
-        //     )
-        // );
+        PengTogether_final vaultImpl = new PengTogether_final();
+        vault.upgradeTo(address(vaultImpl));
 
         // Vault_seth vault_sethImpl = new Vault_seth();
         // vault_seth.upgradeToAndCall(
@@ -62,9 +57,9 @@ contract Upgrade is Script {
         // PengHelperEth pengHelperEthImpl = new PengHelperEth();
         // pengHelperEth.upgradeTo(address(pengHelperEthImpl));
 
-        PengHelperOp pengHelperOpImpl = new PengHelperOp();
-        pengHelperOp.upgradeTo(address(pengHelperOpImpl));
-        address usdc = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
-        pengHelperOp.approveParaswapTokenTransferProxy(usdc);
+        // PengHelperOp pengHelperOpImpl = new PengHelperOp();
+        // pengHelperOp.upgradeTo(address(pengHelperOpImpl));
+        // address usdc = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
+        // pengHelperOp.approveParaswapTokenTransferProxy(usdc);
     }
 }
