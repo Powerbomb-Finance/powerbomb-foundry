73ec1c875679c32a16d7436a198801f2611c635b
Siew <siewchongche@gmail.com>
2023-03-23 20:17:26 +0800

    chore: miscellaneous

--- interfaces/ILendingPool.sol
+++ interfaces/ILendingPool.sol
@@ -3,7 +3,9 @@ pragma solidity 0.8.16;
 
 interface ILendingPool {
     function supply(address asset, uint amount, address onBehalfOf, uint16 referralCode) external;
+
     function withdraw(address asset, uint amount, address to) external;
+    
     function getReserveData(address asset) external view returns (
         uint, uint128, uint128, uint128, uint128, uint128, uint40, uint16, address
     );
--- interfaces/IPair.sol
+++ interfaces/IPair.sol
@@ -6,8 +6,6 @@ import "openzeppelin-contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 interface IPair is IERC20Upgradeable {
     function tokens() external view returns (address, address);
 
-    function claimFees() external;
-
     function stable() external view returns (bool);
 
     function getReserves() external view returns (uint _reserve0, uint _reserve1);
